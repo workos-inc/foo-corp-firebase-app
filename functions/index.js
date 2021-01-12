6b0396052bda148e78499726c6c23468cee961f9
Lo Ferris <lo.ferris@workos.com>
2021-01-12 15:01:51 -0800

    workos side working with OktaSAML

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,18 @@
-require('dotenv');
+const dotenv = require('dotenv');
 const express = require('express');
-const WorkOS = require('@workos-inc/node');
+const WorkOS = require('@workos-inc/node').default;
 const admin = require('firebase-admin');
 
+dotenv.config();
 const app = express();
+const port = process.env.SERVER_PORT;
 
 //WorkOS Logic
 const workos = new WorkOS(process.env.WORKOS_API_KEY);
 const clientID = process.env.WORKOS_CLIENT_ID;
+
+//Firebase logic
+admin.initializeApp();
 
 app.get('/callback', async (req, res) => {
   const { code } = req.query;
@@ -16,11 +21,22 @@ app.get('/callback', async (req, res) => {
     code,
     clientID,
   })
-}
 
-res.redirect('/');
+  admin
+  .auth()
+  .createCustomToken(profile.id)
+  .then((customToken) => {
+    //Send token to client
+    console.log(`${customToken}`);
+  })
+  .catch((error) => {
+    console.log('Error creating custom token: ', error);
+  })
 
-app.get('/auth', {_req, res) => {
+  res.redirect('/');
+});
+
+app.get('/auth', (_req, res) => {
   const domain = process.env.DOMAIN;
   const redirectURI = process.env.REDIRECT_URI;
 
@@ -33,15 +49,7 @@ app.get('/auth', {_req, res) => {
   res.redirect(authorizationURL);
 });
 
-//Firebase logic
-const firebaseApp = admin.initializeApp();
-
-admin
-  .auth()
-  .createCustomToken(clientID)
-  .then((customToken) => {
-    //Send token to client
-  })
-  .catch((error) => {
-    console.log('Error creating custom token: ', error);
-  })
+// start the Express server
+app.listen( port, () => {
+  console.log( `server started at ${port}` );
+} );
